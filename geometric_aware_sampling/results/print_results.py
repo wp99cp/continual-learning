b5ab0d1ff4bac022d139e9ecfd7b7a5c9e2037f0
Cyrill Püntener <wp99cp@gmx.net>
2025-01-05 14:54:13 +0100

    add Top1_Acc forgetting and accuracy plot

--- geometric_aware_sampling/results/print_results.py
+++ geometric_aware_sampling/results/print_results.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from torch.utils.tensorboard import SummaryWriter
 
 from geometric_aware_sampling.results.evolution_graphs.top1_accuracy_evolution_graph import (
@@ -21,5 +22,93 @@ def print_results(overall_results, writer: SummaryWriter):
         std_final_top1_accu = np.array(top1_accuracies[:][-1]).std()
         writer.add_scalar(f"{strategy_name}/mean_Top1_Accuracy", mean_final_top1_acc)
         writer.add_scalar(f"{strategy_name}/std_Top1_Accuracy", std_final_top1_accu)
+
+        __strategy_name = strategy_name.split("__")[-1]
+
+        ########################################################################################################
+        # START: accuracy history plot (only for first task)
+        ########################################################################################################
+
+        # TODO the following only consider the first run
+
+        # class accuracy history plot (only for first task)
+        class_acc_history = []
+
+        for key in results[0].keys():
+            if "Top1_Acc_Exp/eval_phase/test_stream" in key:
+
+                np_array = np.array(results[0][key][1])
+                class_acc_history.append(np_array)
+
+        fig, ax = plt.subplots(figsize=(10, 4), dpi=300)
+
+        for i, class_acc in enumerate(class_acc_history):
+            xs = np.linspace(i, 5, len(class_acc))
+            ax.plot(xs, class_acc, label=f"Testset with classes of task {i}")
+
+        ax.set(
+            xlabel=f"Start Training of Task i",
+            ylabel="Accuracy",
+            title=f"Accuracy History {__strategy_name}",
+        )
+        ax.legend()
+
+        for i in range(1, 5):
+            ax.axvline(x=i, color="k", linestyle="--", alpha=0.25)
+
+        # save the plot to tensorboard
+        writer.add_figure(
+            f"{__strategy_name}/class_accuracy_history", fig, global_step=0
+        )
+
+        ########################################################################################################
+        # END: accuracy history plot (only for first task)
+        ########################################################################################################
+
+        ########################################################################################################
+        # START: forgetting plot
+        ########################################################################################################
+
+        # TODO the following only consider the first run
+
+        # class accuracy history plot (only for first task)
+        class_acc_history = []
+
+        for key in results[0].keys():
+            if "Top1_Acc_Exp/eval_phase/test_stream" in key:
+                np_array = np.array(results[0][key][1])
+                class_acc_history.append(np_array)
+
+        fig, ax = plt.subplots(figsize=(10, 4), dpi=300)
+
+        for i, class_acc in enumerate(class_acc_history):
+            xs = np.linspace(i, 5, len(class_acc))
+
+            train_acc = np.max(class_acc[: len(class_acc) // (5 - i)])
+            forgetting = train_acc - class_acc
+
+            # set forgetting to zero before learning on the experience
+            forgetting[: len(class_acc) // (5 - i)] = 0
+            ax.plot(xs, forgetting, label=f"Testset with classes of task {i}")
+
+        ax.set(
+            xlabel=f"Start Training of Task i",
+            ylabel="Forgetting",
+            title=f"Forgetting {__strategy_name}",
+        )
+
+        ax.legend()
+
+        for i in range(1, 5):
+            ax.axvline(x=i, color="k", linestyle="--", alpha=0.25)
+
+        # show the plot
+
+        # save the plot to tensorboard
+        writer.add_figure(f"{__strategy_name}/forgetting", fig, global_step=0)
+
+        ########################################################################################################
+        # END: forgetting plot
+        ########################################################################################################
 
     print("\n\n\n")
