622602ce7aa2b40bcd6e70700bb4f8165e8905f5
Cyrill Püntener <wp99cp@gmx.net>
2025-01-14 21:19:58 +0100

    add bwt summary script

--- analyse/bwt.py
+++ analyse/bwt.py
@@ -13,7 +13,7 @@ from tbparse import SummaryReader
 
 base_dir = "../tb_data_final_results/"
 
-all_accuracies = {}
+all_bwts = {}
 
 # we take the average over the last 5 test accuracies
 # that is the mean over the values at idx [95, 100], [195, 200], [295, 300]
@@ -34,59 +34,60 @@ for folder in os.listdir(base_dir):
         df = reader.scalars
 
         # get stream with name "Top1_Acc_Exp/eval_phase/test_stream/Task000"
-        top1_acc_exp = df.loc[
-            df["tag"].str.startswith("Top1_Acc_Exp/eval_phase/test_stream/Task000"),
+        bwt_exp = df.loc[
+            df["tag"].str.startswith("StreamBWT/eval_phase/test_stream"),
             "value",
         ].values
 
-        print(f"Calc Top1 Accuracy from stream of Task000")
+        print(f"Calc BWT from stream of Task000")
 
-        mean_accs = []
-        for task in range(3):
+        mean_btws = []
+        for task in range(2, 3):
 
             index_range = task * 100 + 95 if enable_avg_over_last_5 else task * 100 + 99
-            mean_top1_acc = (
-                top1_acc_exp[index_range : index_range + 5].mean().mean()
+            raw_bwts = (
+                bwt_exp[index_range : index_range + 5].mean().mean()
                 if enable_avg_over_last_5
-                else top1_acc_exp[index_range]
+                else bwt_exp[index_range]
             )
 
             # [index_range : index_range + 5].mean()
-            print(f" - Mean Top1 Accuracy for Task{task}: {mean_top1_acc:.2f}")
-            mean_accs.append(mean_top1_acc)
+            print(f" - Mean BWT for Task{task}: {raw_bwts:.2f}")
+            mean_btws.append(raw_bwts)
 
-        mean_accuracy = np.array(mean_accs).mean()
-        print(f"» Mean Top1 Accuracy: {mean_accuracy:.2f}")
+        mean_bwt = np.array(mean_btws).mean()
+        print(f"» Mean BWT: {mean_bwt:.2f}")
 
-        if method_name not in all_accuracies:
-            all_accuracies[method_name] = []
+        if method_name not in all_bwts:
+            all_bwts[method_name] = []
 
-        all_accuracies[method_name].append(mean_accuracy)
+        all_bwts[method_name].append(mean_bwt)
 
     except Exception as e:
         print(f"Error: {e}")
 
 
-# order all_accuracies in the following order: Baseline\_RandomSampling,
+print("\n\n++++++++++++++++++++++\nAll BWTs:")
+
+# order all_bwts in the following order: Baseline\_RandomSampling,
 #  Baseline\_Goldilocks\_RandomSampling, Baseline\_iCaRL\_RandomSampling,
 # GeoAware\_Goldilocks\_WeightedSampling\_Inv, GeoAware\_Goldilocks\_WeightedSampling\_Exp
 
-all_accuracies = {
-    "Baseline_RandomSampling": all_accuracies["Baseline_RandomSampling"],
-    "Baseline_Goldilocks_RandomSampling": all_accuracies[
+all_bwts = {
+    "Baseline_RandomSampling": all_bwts["Baseline_RandomSampling"],
+    "Baseline_Goldilocks_RandomSampling": all_bwts[
         "Baseline_Goldilocks_RandomSampling"
     ],
-    "Baseline_Icarl_RandomSampling": all_accuracies["Baseline_Icarl_RandomSampling"],
-    "GeoAware_Goldilocks_WeightedSampling_Inv": all_accuracies[
+    "Baseline_Icarl_RandomSampling": all_bwts["Baseline_Icarl_RandomSampling"],
+    "GeoAware_Goldilocks_WeightedSampling_Inv": all_bwts[
         "GeoAware_Goldilocks_WeightedSampling_Inv"
     ],
-    "GeoAware_Goldilocks_WeightedSampling_Exp": all_accuracies[
+    "GeoAware_Goldilocks_WeightedSampling_Exp": all_bwts[
         "GeoAware_Goldilocks_WeightedSampling_Exp"
     ],
 }
 
-print("\n\n++++++++++++++++++++++\nAll Accuracies:")
-for method_name, accs in all_accuracies.items():
+for method_name, accs in all_bwts.items():
     print(
-        f"Method: {method_name}, Mean Acc: {np.array(accs).mean():.2f} +/- {np.array(accs).std():.2f}"
+        f"Method: {method_name}, Mean BWT: {np.array(accs).mean():.2f} +/- {np.array(accs).std():.2f}"
     )
